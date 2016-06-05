c019adea2f3f3c340a050a91ab8e66525a2ee08e
SharanyaVR <sharanyavr93@gmail.com>
2016-06-04 19:20:56 -0700

    takes inputs from command line

--- ClusterDetailsExtraction.py
+++ ClusterDetailsExtraction.py
@@ -2,6 +2,33 @@ import pandas
 import operator
 import numpy as np
 import json
+import os
+import glob
+
+pathtoclustermappingfiles = "/home/sharanyavraju/PycharmProjects/Cluster-Mapping-Visualization/ClusterMappingFiles/"
+pathtoClusterDetailFiles = "/home/sharanyavraju/PycharmProjects/Cluster-Mapping-Visualization/ClusterDetailFiles"
+pathtoNodeIdandClusterIdMappingFiles = "/home/sharanyavraju/PycharmProjects/Cluster-Mapping-Visualization/NodeIdandClusterIdMapping"
+listofdirectorypaths = [pathtoclustermappingfiles, pathtoNodeIdandClusterIdMappingFiles, pathtoClusterDetailFiles]
+
+'''
+listofdirectorypaths = [pathtoclustermappingfiles, pathtoNodeIdandClusterIdMappingFiles, pathtoClusterDetailFiles]
+&
+Make sure path ends with '/'
+'''
+listofdirectorypaths = [pathtoclustermappingfiles, pathtoNodeIdandClusterIdMappingFiles, pathtoClusterDetailFiles]
+length = len(listofdirectorypaths)
+for i in range(0, length):
+    path = listofdirectorypaths[i]
+    if path.endswith('/') == False:
+        listofdirectorypaths[i] = path+'/'
+#print listofdirectorypaths
+
+listofclustermappingfiles = os.listdir(listofdirectorypaths[0])
+listofNodeIdandClusterIdMappingFiles = os.listdir(listofdirectorypaths[1])
+listofClusterDetailFiles = os.listdir(listofdirectorypaths[2])
+
+'''
+Creating a list of clustermappingfiles in the following format:
 
 listofclustermappingfiles = [["/home/sharanyavraju/DR/GitHub/dig-coclustering/clustering_results/newest_raw_results/cluster-45/offercluster2attrcluster.txt","O","A"],
                              ["/home/sharanyavraju/DR/GitHub/dig-coclustering/clustering_results/newest_raw_results/cluster-45/offercluster2servicecluster.txt","O","S"],
@@ -11,22 +38,44 @@ listofclustermappingfiles = [["/home/sharanyavraju/DR/GitHub/dig-coclustering/cl
                              ["/home/sharanyavraju/DR/GitHub/dig-coclustering/clustering_results/newest_raw_results/cluster-45/webpagecluster2featurecluster.txt","P","F"],
                              ]
 
+'''
+lengthoflist = len(listofclustermappingfiles)
+for i in range(0,lengthoflist):
+    listofclustersinmapping = str.split(listofclustermappingfiles[i], '2')
+    listofclustermappingfiles[i] = [listofdirectorypaths[0]+listofclustermappingfiles[i], listofclustersinmapping[0][0],listofclustersinmapping[1][0]]
+#print  listofclustermappingfiles
+
+nodedict = {}
+for EveryNodeIdandCluterIdMapping in listofNodeIdandClusterIdMappingFiles:
+    nodeName = str.split(EveryNodeIdandCluterIdMapping,"cluster")[0]
+    nodedict[nodeName] = EveryNodeIdandCluterIdMapping
+
+'''
+Creating a list of ClusterDetailandIndexFiles in the following format
+
 listofClusterDetailAndIndexFiles = [["/home/sharanyavraju/DR/GitHub/dig-coclustering/graph-files/detail/attrdetail.txt", "/home/sharanyavraju/DR/GitHub/dig-coclustering/clustering_results/newest_raw_results/cluster-45/attrcluster.txt","A"],
                                     ["/home/sharanyavraju/DR/GitHub/dig-coclustering/graph-files/index/wordindex.txt", "/home/sharanyavraju/DR/GitHub/dig-coclustering/clustering_results/newest_raw_results/cluster-45/featurecluster.txt","F"],
                                     ["/home/sharanyavraju/DR/GitHub/dig-coclustering/graph-files/index/offerindex.txt","/home/sharanyavraju/DR/GitHub/dig-coclustering/clustering_results/newest_raw_results/cluster-45/offercluster.txt","O"],
                                     ["/home/sharanyavraju/DR/GitHub/dig-coclustering/graph-files/detail/pagedetail.txt","/home/sharanyavraju/DR/GitHub/dig-coclustering/clustering_results/newest_raw_results/cluster-45/webpagecluster.txt","P"],
                                     ["/home/sharanyavraju/DR/GitHub/dig-coclustering/graph-files/detail/servicedetail.txt","/home/sharanyavraju/DR/GitHub/dig-coclustering/clustering_results/newest_raw_results/cluster-45/servicecluster.txt","S"],
                                     ]
+'''
+listofClusterDetailAndIndexFiles = []
+for EveryClusterDetailFile in listofClusterDetailFiles:
+    nodeName = str.split(EveryClusterDetailFile, "detail")[0]
+    listofClusterDetailAndIndexFiles.append([listofdirectorypaths[2]+EveryClusterDetailFile, listofdirectorypaths[1]+nodedict.get(nodeName), nodeName[0]])
+
+# print listofClusterDetailAndIndexFiles
 
 IdValueMapping = {}
 DictofPropertyDetails = {}
 
 def getDetailsaAboutCluster(indexfile, clusterfile):
-    print indexfile +" ____ " + clusterfile
+    #print indexfile +" ____ " + clusterfile
     with open(indexfile) as clusterdetails:
         for line in clusterdetails:
             word = line.split()
-            print str(word) + "---" + str(line)
+            #print str(word) + "---" + str(line)
             IdValueMapping[int(word[0])+1] = word[1]
 
     ClusterDataFrame = pandas.read_csv(clusterfile, "\t", header=None)
@@ -92,7 +141,7 @@ def main():
         linkdictlist += linkdictlist1
 
     nodedictlist = list(np.unique(np.array(nodedictlist)))
-    print str(nodedictlist)
+    #print str(nodedictlist)
 
     for dic in linkdictlist:
         dic["source"] = nodedictlist.index([x for x in nodedictlist if x['name'] == dic["source"]][0])
